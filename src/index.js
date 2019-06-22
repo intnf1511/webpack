c6b1ac0d4c52ab349612c57c949b40342b181d56
NF <intnf1511@gmail.com>
2019-06-22 11:21:13 +0900

    rrrrr

---
+++ src/index.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');  /*CommonJS 스펙의 모듈 가져오기*/
+
+// named import : {} 안에 이름을 명시
+import {area, circumference} from'./js/circle';
+
+//default import 방법 : {} 없이, 이름을 임의로 변경 가능
+import aaa from'./js/cubic';
+
+import './style.css';
+import './hello.scss';
+
+function component() {
+    let element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+console.log('webpack test');
+console.log(area(5), circumference(10));
+console.log('-------------------------------------');
+console.log(aaa(20));
+
+document.body.appendChild(component());
